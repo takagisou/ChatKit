9e20b9e76aedb5ea8b85ef876ee455dabbcd4b4b
sana <takagisou@users.noreply.github.com>
2020-05-29 06:59:44 +0900

    convert to kotlin

--- sample/src/main/java/com/stfalcon/chatkit/sample/common/data/model/Message.kt
+++ sample/src/main/java/com/stfalcon/chatkit/sample/common/data/model/Message.kt
@@ -12,12 +12,13 @@ class Message @JvmOverloads constructor(
         override val user: User,
         override var text: String?,
         override var createdAt: Date = Date()) : IMessage, MessageContentType.Image, MessageContentType /*and this one is for custom content type (in this case - voice message)*/ {
-    private var image: Image? = null
-    var voice: Voice? = null
 
-    override fun getImageUrl(): String? {
-        return if (image == null) null else image!!.url
-    }
+    override val imageUrl: String?
+        get() = image?.url
+
+    private var image: Image? = null
+
+    var voice: Voice? = null
 
     val status: String
         get() = "Sent"
